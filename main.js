90efced3cc7df5ae0ca73b6c849dcedf95637874
Matthew Yamout <matthewyamout@Matthews-MacBook-Air.local>
2018-01-25 12:00:46 -0800

    Added babel dependencies to run ES6 with Node

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-var RequestNetwork = require('./dist/src/requestNetwork').default;
+import RequestNetwork from './dist/src/requestNetwork';
+import Eth from 'ethjs';
 
-var rn = new RequestNetwork('http://localhost:8545', 45, false);
+const rn = new RequestNetwork('http://localhost:8545', 45, false);
+const eth = new Eth(new Eth.HttpProvider('http://localhost:8545'));
+let accounts = [];
+
+const mainAsync = async () => {
+    try {
+        accounts = await eth.accounts();
+        console.log(accounts);
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+mainAsync().catch((error) => {
+    console.log(error);
+});
